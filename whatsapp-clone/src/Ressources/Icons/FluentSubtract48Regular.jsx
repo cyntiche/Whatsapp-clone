30b7ef8504f84bc5c1f6f9cda9466358a944fa3f
Nguemete Cyntiche <cyntichenguemete@gmail.com>
2024-02-13 17:06:47 +0100

    Initialisation of project and creation of  nav bar and side bar

---
+++ whatsapp-clone/src/Ressources/Icons/FluentSubtract48Regular.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function FluentSubtract48Regular(props) {
+	return (<svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 48 48" {...props}><path fill="currentColor" d="M6 23.25c0-.69.56-1.25 1.25-1.25h33.5a1.25 1.25 0 1 1 0 2.5H7.25c-.69 0-1.25-.56-1.25-1.25"></path></svg>);
+}
