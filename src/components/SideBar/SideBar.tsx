8776b9bdf4caf5285aee5a778f4123af878e9656
Nguemete Cyntiche <cyntichenguemete@gmail.com>
2024-02-15 14:18:02 +0100

    refactor(app): #3 Change js to ts, refactor icons, use Mui

--- src/components/SideBar/SideBar.tsx
+++ src/components/SideBar/SideBar.tsx
@@ -1,31 +1,95 @@
-import React, { useState } from "react";
-import "./Side-bar.css";
-import FluentChat16Regular from "../../Ressources/Icons/FluentChat16Regular";
-import FluentCall16Regular from "../../Ressources/Icons/FluentCall16Regular";
-import FluentHeartCircleHint16Regular from "../../Ressources/Icons/FluentHeartCircleHint16Regular";
-import FluentStar16Regular from "../../Ressources/Icons/FluentStar16Regular";
-import FluentArchive16Regular from "../../Ressources/Icons/FluentArchive16Regular";
-import FluentSettings16Regular from "../../Ressources/Icons/FluentSettings16Regular";
+import Archive from "@iconify/icons-fluent/archive-24-regular";
+import Call from "@iconify/icons-fluent/call-24-regular";
+import Chat from "@iconify/icons-fluent/chat-24-regular";
+import HeartCircleHint from "@iconify/icons-fluent/heart-circle-hint-24-regular";
+import Settings from "@iconify/icons-fluent/settings-24-regular";
+import Star from "@iconify/icons-fluent/star-24-regular";
+import { Icon, IconifyIcon } from "@iconify/react";
+import { Box, Tooltip } from "@mui/material";
+import Avatar from "@mui/material/Avatar";
+import Stack from "@mui/material/Stack";
 
 const SideBar = () => {
-      const [avatar, setAvatar] = useState(require("../../Ressources/avatar.png"));
+  const iconsTop: { title: string; icon: IconifyIcon; color: string }[] = [
+    {
+      title: "Chat",
+      icon: Chat,
+      color: "white",
+    },
+    {
+      title: "Call",
+      icon: Call,
+      color: "white",
+    },
+    {
+      title: "Story",
+      icon: HeartCircleHint,
+      color: "white",
+    },
+  ];
+  const iconsBotton: { title: string; icon: IconifyIcon; color: string }[] = [
+    {
+      title: "Star",
+      icon: Star,
+      color: "white",
+    },
+    {
+      title: "Archive",
+      icon: Archive,
+      color: "white",
+    },
+    {
+      title: "Settings",
+      icon: Settings,
+      color: "white",
+    },
+  ];
 
   return (
-    <div className="sidebar">
-      <div className="icons-top">
-        <span className="sidebar-icon active">{FluentChat16Regular()}</span>
-        <span className="sidebar-icon">{FluentCall16Regular()}</span>
-        <span className="sidebar-icon">{FluentHeartCircleHint16Regular()}</span>
-      </div>
-      <div className="icons-bottom">
-        <span className="sidebar-icon">{FluentStar16Regular()}</span>
-        <span className="sidebar-icon">{FluentArchive16Regular()}</span>
-        <span className="sidebar-icon">{FluentSettings16Regular()}</span>
-        <span className="sidebar-avatar"><img src={avatar} alt="Photo de profil" /></span>
-        <span className="sidebar-icon">{FluentSettings16Regular()}</span>
-
-      </div>
-    </div>
+    <Box
+      sx={{
+        display: "grid",
+        height: "100%",
+        width: "40px",
+        backgroundColor: "#202020",
+        alignItems: "center",
+        alignContent: "space-between",
+      }}
+    >
+      <Box
+        sx={{
+          display: "grid",
+          flexDirection: "column",
+          justifyItems: "center",
+          alignContent: "flex-start",
+          paddingTop: "14px",
+          gap: 3,
+        }}
+      >
+        {iconsTop.map(({ title, icon, color }, index) => (
+          <Tooltip arrow title={title}>
+            <Icon icon={icon} color={color} />
+          </Tooltip>
+        ))}
+      </Box>
+      <Box
+        sx={{
+          display: "grid",
+          flexDirection: "column",
+          justifyItems: "center",
+          alignContent: "flex-end",
+          paddingBottom: "10px",
+          gap: 3,
+        }}
+      >
+        {iconsBotton.map(({ title, icon, color }, index) => (
+          <Tooltip arrow title={title}>
+            <Icon icon={icon} color={color} />
+          </Tooltip>
+        ))}
+          <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" sx={{height:'24px', width:'24px'}}/>
+      </Box>
+    </Box>
   );
 };
 
