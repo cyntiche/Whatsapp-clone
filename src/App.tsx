c5becdd1270942ba1fe4134df9911f98bd030c2e
Nguemete Cyntiche <cyntichenguemete@gmail.com>
2024-02-21 11:52:40 +0100

    fix(app): #3 rename chat-section to ChatSection

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { Box } from '@mui/material';
-import ChatSection from './components/Chat-section/ChatSection';
+import ChatSection from './components/ChatSection/ChatSection';
 import Contacts from './components/Contacts/Contacts';
 import SideBar from './components/SideBar/SideBar';
 import TitleBar from './components/TitleBar/TitleBar';
